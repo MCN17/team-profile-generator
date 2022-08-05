cdb52451a8e411807f618f7124a4040aa406161c
Marty <martynolan17@gmail.com>
2022-08-05 12:10:34 -0400

    test for intern passes

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,13 @@
+const Intern = require("../lib/Intern")
+
+test("adds an Intern", () => {
+    const intern = new Intern("Bruster", "8", "bruster@gmail.com", "Carleton")
+
+    expect (intern.getRole()).toBe("Intern");
+})
+
+test("adds school", () => {
+    const intern = new Intern("Bruster", "8", "bruster@gmail.com", "Carleton")
+
+    expect(intern.school).toBe("Carleton");
+})
